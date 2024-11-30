25113f334cfcd739d2eb25d89f9b9662fc879f58
Nikita <134389334+NikiWeasel@users.noreply.github.com>
2024-11-30 16:52:28 +1000

    fetch_regulations_bloc создан

---
+++ lib/core/bloc/fetch_regulations/fetch_regulations_bloc.dart
@@ -0,0 +1,51 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
+
+import '../../models/regulation.dart';
+
+part 'fetch_regulations_event.dart';
+
+part 'fetch_regulations_state.dart';
+
+class FetchRegulationsBloc
+    extends Bloc<FetchRegulationsEvent, FetchRegulationsState> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  FetchRegulationsBloc() : super(FetchRegulationsInitialState()) {
+    on<FetchRegulationsData>(_onFetchRegulationsData);
+  }
+
+  Future<void> _onFetchRegulationsData(
+      FetchRegulationsData event, Emitter<FetchRegulationsState> emit) async {
+    print('bloc triggered');
+
+    emit(FetchRegulationsLoadingState());
+
+    try {
+      final QuerySnapshot snapshot =
+          await _firestore.collection('appointments').get();
+
+      final List<Regulation> allAppointments = snapshot.docs.map((doc) {
+        final data = doc.data() as Map<String, dynamic>;
+
+        return Regulation(
+            name: data['masterId'],
+            duration: data['clientName'],
+            cost: data['clientNumber'],
+            id: doc.id);
+      }).toList();
+
+      if (allAppointments.isEmpty) {
+        emit(FetchRegulationsErrorState(
+            errorMessage: 'Данные регламентов не найдены'));
+        return;
+      }
+      List<Regulation> newList = List.from(allAppointments);
+
+      emit(FetchRegulationsLoadedState(regulations: newList));
+    } catch (e) {
+      emit(FetchRegulationsErrorState(errorMessage: e.toString()));
+    }
+  }
+}
---
+++ lib/core/bloc/fetch_regulations/fetch_regulations_event.dart
@@ -0,0 +1,6 @@
+part of 'fetch_regulations_bloc.dart';
+
+@immutable
+sealed class FetchRegulationsEvent {}
+
+class FetchRegulationsData extends FetchRegulationsEvent {}
